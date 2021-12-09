27e8711868ea4f4fbf7ddb77596ff2d72f3d38c8
kaitsubaka <aj.ayte@uniandes.edu.co>
2021-12-08 19:31:59 -0500

    code -> before xml implementation

--- amplify/backend/function/Randomizer/src/routes/randomizer.route.js
+++ amplify/backend/function/Randomizer/src/routes/randomizer.route.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const randomize = require('../controllers/randomizer.ctl');
-const router = new express.Router();
+const express = require('express')
+const randomize = require('../controllers/randomizer.ctl')
+const router = new express.Router()
 
 /*
- * get all estudiantes in the system
+ * get a random meditation form the meditation framework
+ * definition: /randomize?time={time}&avg={averageCharPerSec}
+ * time => meditation's duration in minutes
+ * averageTimePerLetter
+ * response body:
+    {
+    "step00": [
+            {
+                "_id":"6169c53d201ebe4f6a58c76a",
+                "content":"bienvenido a gradua..."
+            }
+        ],
+    ...
+    "step7": [
+            {
+                "_id":"6169ba2036651c7498ee7d5c",
+                "content":"gracias por meditar con nosotros...",
+            }
+        ],
+    }
  */
-router.get('/', randomize);
+router.get('/', randomize)
 
-module.exports = router;
+module.exports = router
