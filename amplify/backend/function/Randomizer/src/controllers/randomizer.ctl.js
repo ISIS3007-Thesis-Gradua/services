6823941e475c7c6aecb5d2e970430d50d852f74b
kaitsubaka <aj.ayte@uniandes.edu.co>
2021-12-08 19:31:30 -0500

    code -> before xml implementation

--- amplify/backend/function/Randomizer/src/controllers/randomizer.ctl.js
+++ amplify/backend/function/Randomizer/src/controllers/randomizer.ctl.js
@@ -1,49 +1,116 @@
 // eslint-disable-next-line import/no-absolute-path
 const connectToDatabase = require('../schemas')
+const { XMLParser } = require('fast-xml-parser')
+const getTimeFromSSML = (text) => {
+  // This is just for getting the numbers in te String. Shouldn't be more than 1 chain of numbers
+  const numbers = /d+/
+  // This regex supports spaces between the number and the seconds key "s".
+  const seconds = /d+s*s/
 
+  const miliseconds = /d+s*ms/
+  console.log()
+  const [matched] = text.match(numbers)
+  const parsedNumbers = parseInt(matched ?? '100')
+  if (text.test(miliseconds)) {
+    return 1000 * parsedNumbers
+  } else if (text.test(seconds)) {
+    return parsedNumbers
+  } else {
+    return 1000 * parsedNumbers
+  }
+}
 const randomize = async (req, res) => {
   const {
     models: { Step00, Step01, Step1, Step2, Step3, Step4, Step5, Step6, Step7 }
   } = await connectToDatabase()
 
   try {
-    const { time = 0, avg = 5 } = req.query
+    // ============================================================
+    // ========================variables===========================
+    // ============================================================
+    const { time = 0, avgWordsPerMin = 170, charsPerWord = 10 } = req.query // the default values matches spanish
+    const avgTimePerChar = (avgWordsPerMin * charsPerWord) / 60
+    console.log(avgTimePerChar)
+    const maxTime = time * 60
+    let currenTime = 0
+    const stepNames = {
+      step00: 'step00',
+      step01: 'step01',
+      step1: 'step1',
+      step2: 'step2',
+      step3: 'step3',
+      step4: 'step4',
+      step5: 'step5',
+      step6: 'step6',
+      step7: 'step7'
+    }
     const routine = {
-      step00: await Step00.aggregate([{ $sample: { size: 1 } }]),
-      step01: await Step01.aggregate([{ $sample: { size: 1 } }]),
-      step1: await Step1.aggregate([{ $sample: { size: 1 } }]),
-      step2: [],
-      step3: [],
-      step4: [],
-      step5: [],
-      step6: await Step6.aggregate([{ $sample: { size: 1 } }]),
-      step7: await Step7.aggregate([{ $sample: { size: 1 } }])
+      [stepNames.step00]: [],
+      [stepNames.step01]: [],
+      [stepNames.step1]: [],
+      [stepNames.step2]: [],
+      [stepNames.step3]: [],
+      [stepNames.step4]: [],
+      [stepNames.step5]: [],
+      [stepNames.step6]: [],
+      [stepNames.step7]: []
     }
-    const reduceToLen = (curr, doc) => curr + doc.content.length
-    const totalLetters = (time || 0) * (avg || 0)
-    let routineLeght = Object.values(routine).reduce(
-      (curr, step) => curr + step.reduce(reduceToLen, 0),
-      0
-    )
+    // ============================================================
+    // ========================functions===========================
+    // ============================================================
+    // retrives the content from a given step and inserts it into the response object
+    const updateStep = async (Step, stepName) => {
+      // calculates the time of a given ssml string
+      const reduceToTime = (prev, curr) => {
+        const tagsTime = getTimeFromSSML(curr.content)
+        const textTime = curr.content.length
+        console.log('===>> ', curr.content, '===', textTime)
+        return prev + tagsTime + textTime
+      }
+      // get the step
+      const step = await Step.aggregate([{ $sample: { size: 1 } }])
+      // set the new content
+      routine[stepName] = [...routine[stepName], ...step]
+      // return the time of the step
+      return routine[stepName].reduce(reduceToTime, 0)
+    }
+    // returns true if the total time of the generated meditation is greater than maxTime
+    const checkTimeLimitReached = () => currenTime <= maxTime * 0.9
+
+    // ============================================================
+    // ========================algorithm===========================
+    // ============================================================
+
+    // retrives welcome message
+    currenTime += await updateStep(Step00, stepNames.step00)
+    // retrives the before start message
+    currenTime += await updateStep(Step01, stepNames.step01)
+    // retrives the first step
+    currenTime += await updateStep(Step1, stepNames.step1)
+    // retrives the sixth step
+    currenTime += await updateStep(Step6, stepNames.step6)
+    // retrives the last step
+    currenTime += await updateStep(Step7, stepNames.step7)
+    // randomly chose if the second step will be retrived
     if (Math.floor(Math.random() * 2) === 1) {
-      routine.step2 = await Step2.aggregate([{ $sample: { size: 1 } }])
-      routineLeght += routine.step2.reduce(reduceToLen, 0)
+      currenTime += await updateStep(Step2, stepNames.step2)
     }
-    do {
-      const step3 = await Step3.aggregate([{ $sample: { size: 1 } }])
-      routine.step3 = [...routine.step3, ...step3]
-      routineLeght += step3.reduce(reduceToLen, 0)
+    // with the rest of the time we create a loop for the procedural
+    // generation of the step sequece 3,4,5
+    // stopping in any part of the procedural generation if the threshold is reached
+    while (checkTimeLimitReached()) {
+      currenTime += await updateStep(Step3, stepNames.step3)
+      if (!checkTimeLimitReached()) {
+        break
+      }
+      currenTime += await updateStep(Step4, stepNames.step4)
+      if (!checkTimeLimitReached()) {
+        break
+      }
+      currenTime += await updateStep(Step5, stepNames.step5)
+    }
 
-      const step4 = await Step4.aggregate([{ $sample: { size: 1 } }])
-      routine.step4 = [...routine.step4, ...step4]
-      routineLeght += step4.reduce(reduceToLen, 0)
-
-      const step5 = await Step5.aggregate([{ $sample: { size: 1 } }])
-      routine.step5 = [...routine.step5, ...step5]
-      routineLeght += step5.reduce(reduceToLen, 0)
-    } while (routineLeght <= totalLetters * 0.9)
-
-    res.status(200).send(routine)
+    res.status(200).send({ routine: routine, totalTime: currenTime })
   } catch (err) {
     console.log(err)
     res.status(404).send({ message: err.message })
